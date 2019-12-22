97db026a4ff4caa0c994e65ef486a388a46137da
Леонид <leonid20.0831@gmail.com>
2019-12-22 21:21:58 +0300

    fix

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main(){
-    clock_t start = clock();
-
     int m = 100, n = 3500, W = 100000;
-
     vector<pair<int, pair<int,int> > > g;
 
-    vector<vector<int> > graph(n, vector<int> (n, 0));
-
-    //Generate a random graph as a list of edges
+    //Генерация рандомного графа в виде списка ребер
     for(int i = 0; i<n; i++)
         for(int j = i+1; j<n; j++)
                 g.push_back({rand() % W, {i, j}});
 
-    //Kraskal Algorithm
+    cout << g.size() << endl;
+    //Алгоритм Краскала
     int cost = 0;
     vector < pair<int,int> > res;
 
+    clock_t start = clock();
+
     sort (g.begin(), g.end());
+
     vector<int> tree_id(n);
     for (int i=0; i<n; ++i)
     	tree_id[i] = i;
+
     for (int i=0; i<g.size(); ++i)
     {
     	int a = g[i].second.first,  b = g[i].second.second,  l = g[i].first;
@@ -38,18 +38,19 @@ int main(){
     	}
     }
 
-    //Display the number of ribs in the minimum skeleton
-    cout << res.size() << "\n\n";
+    clock_t end = clock();
 
-    //Р’С‹РІРѕРґ РІСЃРµС… СЂРµР±РµСЂ
+    //Вывод количества ребер в минимальном остове
+    //cout << res.size() << "\n\n";
+
+    //Вывод всех ребер
     /*
     for (int i = 0; i<res.size(); i++)
         cout << i << ' ' << res[i].first << ' ' << res[i].second << '\n';
     */
 
-    clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    cout << seconds << '\n';
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC * 1000000;
+    cout << fixed << "Seconds: " << seconds << '\n';
 
     cin >> n;
 }
